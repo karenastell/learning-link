60568262b39ec28258b031b0768b3aeb529400a0
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-17 12:42:51 -0400

    added props and axios to the page files

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import axios from 'axios';
 
-export default function Main(){
+export default function Main(props){
     return 
 }
--- client/src/pages/MyProfile.js
+++ client/src/pages/MyProfile.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import axios from 'axios';
 
-export default function MyProfile(){
+export default function MyProfile(props){
     return 
 }
