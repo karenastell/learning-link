9f6227defb4fad767547dce564de567262a20b2a
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-21 10:03:17 -0400

    set up the signup api route on the backend- woohoo

--- models/user.js
+++ models/user.js
@@ -81,10 +81,11 @@ module.exports = function (sequelize, DataTypes) {
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
   User.addHook('beforeCreate', (user) => {
+    // eslint-disable-next-line no-param-reassign
     user.password = bcrypt.hashSync(
       user.password,
       bcrypt.genSaltSync(10),
-      null
+      null,
     );
   });
   return User;
