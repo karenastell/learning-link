3d2e578bd3357be3638024e5efecc02cec47ccde
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-21 10:37:22 -0400

    added state to handle the teacher input form

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,21 +2,23 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <footer class="foot has-text-centered mt-6">
+        <footer className="foot has-text-centered mt-6">
         <h2>Learning Link</h2>
         <p>
           &copy; 2020
           <a
-            class="hover footer-links"
+            className="hover footer-links"
             href="https://github.com/Kaleighspurio"
             target="_blank"
+            rel="noopener noreferrer"
             > Kaleigh </a
           >
            &  
           <a
-            class="hover footer-links"
+            className="hover footer-links"
             href="https://github.com/karenastell"
             target="_blank"
+            rel="noopener noreferrer"
             > Karen</a
           >
         </p>
