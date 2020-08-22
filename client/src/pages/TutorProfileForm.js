2aac624e9a37c6455a5895687b16f3f1812b6ef0
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-22 10:08:02 -0400

    fixed async difficulties with posting new user in the backend

--- client/src/pages/TutorProfileForm.js
+++ client/src/pages/TutorProfileForm.js
@@ -36,7 +36,7 @@ export default function ProfileForm(props) {
     ])
   }
 
-  const onButtonSubmit = (event) => {
+  const tutorOnButtonSubmit = (event) => {
     // for now, we'll put event.preventDefault(), but eventually we will redirect the user
     event.preventDefault();
     Axios.post('/api/auth/signup-tutor', {
@@ -55,7 +55,7 @@ export default function ProfileForm(props) {
       days: days,
     }).then((response) => {
       console.log(response);
-      // then redirect
+      // TODO: then redirect
     })
   }
 
@@ -205,7 +205,16 @@ export default function ProfileForm(props) {
         </div>
       </div>
 
-      <Button onButtonSubmit={onButtonSubmit} />
+      <div className='field is-horizontal'>
+        <div className='field-label'></div>
+        <div className='field-body'>
+          <div className='field'>
+            <div className='control'>
+              <button className='button is-primary' onClick={tutorOnButtonSubmit}>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
