4a4dda97845bac11df22362f8f746e5c7f94e072
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-24 16:53:48 -0400

    added scrolling to the editprofile components and updatemessage page

--- client/src/components/EditSubjects.js
+++ client/src/components/EditSubjects.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import Subjects from '../components/Subjects';
 import { AuthContext } from '../AuthContext';
@@ -14,6 +14,13 @@ export default function EditSubjects({ setEditSubjectsMode, isTeacher }) {
   const handleCheckboxes = (event) => {
     setSubjects([...subjects, event.target.value]);
   };
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }, []);
 
   const history = useHistory();
 
--- client/src/pages/UpdatedProfileMessage.js
+++ client/src/pages/UpdatedProfileMessage.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Axios from 'axios';
 import SideBarMenu from '../components/SideBarMenu';
 
 export default function UpdateProfileMessage(props) {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }, []);
   return (
     <>
       <div className="columns">
         <SideBarMenu />
         <div className="container">
-          <article class="message is-primary is-large">
-            <div class="message-body">Your profile has been updated!</div>
+          <article className="message is-primary is-large">
+            <div className="message-body">Your profile has been updated!</div>
           </article>
-          <div className="columns is-centered">
+          {/* <div className="columns is-centered">
             <Link to='/myprofile' className="column button is-primary is-half">View My Profile</Link>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
