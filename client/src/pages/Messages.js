b2bcaa433e0539a08aa5bdfffb77f5481b05d4bb
Karen Astell <KarenAstell@gmail.com>
2020-08-29 14:05:31 -0400

    added to post function to button click to post message

--- client/src/pages/Messages.js
+++ client/src/pages/Messages.js
@@ -22,7 +22,7 @@ export default function Messages({ location }) {
   useEffect(() => {
     const { user1, user2 } = queryString.parse(location.search);
     const startFunction = async () => {
-      const { user1, user2 } = queryString.parse(location.search);
+    const { user1, user2 } = queryString.parse(location.search);
 
       if (isTeacher) {
         const messageRoomInfo = await Axios.get(
@@ -70,11 +70,18 @@ export default function Messages({ location }) {
 
   const postMessage = (event) => {
     event.preventDefault();
+    const { user1, user2 } = queryString.parse(location.search);
     sendMessage();
     if (isTeacher) {
-      Axios.post(`/api/message/tutor${user1}/student${user2}`, message);
+      Axios.post(
+        `/api/message/tutor${user1}/student${user2}/sender${user1}/room${room}`,
+        message
+      );
     } else {
-      Axios.post(`/api/message/tutor${user2}/student${user1}`, message);
+      Axios.post(
+        `/api/message/tutor${user2}/student${user1}/sender${user1}/room${room}`,
+        message
+      );
     }
   };
 
@@ -116,10 +123,10 @@ export default function Messages({ location }) {
               value={message}
               onChange={(event) => setMessage(event.target.value)}
               onKeyPress={(event) =>
-                event.key === 'Enter' ? sendMessage(event) : null
+                event.key === 'Enter' ? postMessage(event) : null
               }
             />
-            <button className='button' onClick={(event) => sendMessage(event)}>
+            <button className='button' onClick={(event) => postMessage(event)}>
               Send
             </button>
           </form>
