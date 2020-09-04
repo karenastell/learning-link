83297f64269c6e0ea87fb103656e2cbd13fefb56
Karen Astell <KarenAstell@gmail.com>
2020-09-03 20:07:40 -0400

    if unread messages the state will update

--- client/src/components/DashboardCard/DashboardCard.js
+++ client/src/components/DashboardCard/DashboardCard.js
@@ -11,6 +11,7 @@ export default function DashboardCard({ result, getMyStudentTutorPairs }) {
   const [reviewer, setReviewer] = useState({});
   const [removeMessage, setRemoveMessage] = useState('modal');
   const [room, setRoom] = useState('');
+  const [unread, setUnread] = useState(false);
 
   const [emptyReviewMessage, setEmptyReviewMessage] = useState('off');
 
@@ -108,18 +109,33 @@ export default function DashboardCard({ result, getMyStudentTutorPairs }) {
     }
   };
 
+  const checkForUnreadMessages = () => {
+    Axios.get(
+      `/api/unread/${userId}/${result.id}/${isTeacher}`
+    ).then((response) => {
+      console.log(response);
+      if(response.data.length > 0){
+        setUnread(true)
+      }
+    });
+  
+  };
+
+  checkForUnreadMessages();
+console.log(unread);
+
   return (
     <>
-      <div className="column is-half-tablet is-one-third-desktop">
-        <div className="card mb-6 card-style">
-          <header className="card-header card-head-style">
-            <p className="column">
-              {result.firstName} {result.lastName}
+      <div className='column is-half-tablet is-one-third-desktop'>
+        <div className='card mb-6 card-style'>
+          <header className='card-header card-head-style'>
+            <p className='column'>
+              {result.firstName} {result.lastName} <p className='fas fa-circle'></p>
               <br />
             </p>
           </header>
-          <div className="card-content is-size-7 card-body-style">
-            <div className="content">
+          <div className='card-content is-size-7 card-body-style'>
+            <div className='content'>
               <ul>
                 <li>Email: {result.email}</li>
                 {!isTeacher ? (
@@ -150,20 +166,30 @@ export default function DashboardCard({ result, getMyStudentTutorPairs }) {
               </ul>
             </div>
           </div>
-          <footer className="card-footer card-foot">
-            <div className="buttons card-buttons">
-              <Link
-                to={`/message?user1=${userId}&user2=${result.id}&room=${room}`}
-                className="card-footer-item card-buttons button is-info is-light is-size-7 is-white"
-                onClick={setMessageRoom}
-              >
-                Message {result.firstName}
-              </Link>
+          <footer className='card-footer card-foot'>
+            <div className='buttons card-buttons'>
+              {unread === true ? (
+                <Link
+                  to={`/message?user1=${userId}&user2=${result.id}&room=${room}`}
+                  className='card-footer-item card-buttons button is-info is-light is-size-7 is-white'
+                  onClick={setMessageRoom}
+                >
+                  <i className='fas fa-circle'></i> Message {result.firstName}
+                </Link>
+              ) : (
+                <Link
+                  to={`/message?user1=${userId}&user2=${result.id}&room=${room}`}
+                  className='card-footer-item card-buttons button is-info is-light is-size-7 is-white'
+                  onClick={setMessageRoom}
+                >
+                  Message {result.firstName}
+                </Link>
+              )}
 
               {!isTeacher ? (
                 <button
                   onClick={activateModal}
-                  className="card-footer-item card-buttons button is-size-7 is-white is-info is-light"
+                  className='card-footer-item card-buttons button is-size-7 is-white is-info is-light'
                 >
                   Leave a Review
                 </button>
@@ -171,16 +197,16 @@ export default function DashboardCard({ result, getMyStudentTutorPairs }) {
               {!isTeacher ? (
                 <button
                   onClick={handleReadReview}
-                  className="card-footer-item card-buttons button is-size-7 is-white is-info is-light"
+                  className='card-footer-item card-buttons button is-size-7 is-white is-info is-light'
                 >
                   See {result.firstName}'s Reviews
                 </button>
               ) : null}
               <button
-                className="card-footer-item card-buttons button is-size-7 is-white is-info is-light"
+                className='card-footer-item card-buttons button is-size-7 is-white is-info is-light'
                 onClick={handleRemoveModal}
               >
-                Remove {result.firstName}
+                Remove {result.firstName} 
               </button>
             </div>
           </footer>
@@ -189,54 +215,54 @@ export default function DashboardCard({ result, getMyStudentTutorPairs }) {
 
       {/* Write a Review Modal */}
       <div className={reviewModal}>
-        <div className="modal-background"></div>
-        <div className="modal-card">
-          <header className="modal-card-head modal-header-style">
-            <p className="modal-card-title">
+        <div className='modal-background'></div>
+        <div className='modal-card'>
+          <header className='modal-card-head modal-header-style'>
+            <p className='modal-card-title'>
               Write a review for {result.firstName} {result.lastName}
             </p>
             <button
-              className="delete"
-              aria-label="close"
+              className='delete'
+              aria-label='close'
               onClick={handleModalClose}
             ></button>
           </header>
-          <section className="modal-card-body">
+          <section className='modal-card-body'>
             {emptyReviewMessage === 'on' ? (
-              <article className="message is-danger">
-                <div className="message-body">
+              <article className='message is-danger'>
+                <div className='message-body'>
                   Oops! You cannon submit an empty review!
                 </div>
               </article>
             ) : null}
             <textarea
-              className="textarea"
-              name="review"
+              className='textarea'
+              name='review'
               value={review.review || undefined}
-              placeholder="Leave your review here..."
+              placeholder='Leave your review here...'
               onChange={handleInputChange}
             ></textarea>
-            <div className="field">
-              <p className="control is-expanded has-icons-left my-4">
-                <label className="label" htmlFor="Name">
+            <div className='field'>
+              <p className='control is-expanded has-icons-left my-4'>
+                <label className='label' htmlFor='Name'>
                   Your Name
                   <input
-                    className="input"
-                    id="Name"
-                    type="text"
-                    placeholder="Your Name"
-                    name="Name"
+                    className='input'
+                    id='Name'
+                    type='text'
+                    placeholder='Your Name'
+                    name='Name'
                     onChange={handleReviewerInputChange}
                   />
                 </label>
               </p>
             </div>
           </section>
-          <footer className="modal-card-foot modal-bottom-style">
-            <button className="button is-info" onClick={handleReviewSubmit}>
+          <footer className='modal-card-foot modal-bottom-style'>
+            <button className='button is-info' onClick={handleReviewSubmit}>
               Submit Review
             </button>
-            <button className="button" onClick={handleModalClose}>
+            <button className='button' onClick={handleModalClose}>
               Cancel
             </button>
           </footer>
@@ -245,30 +271,30 @@ export default function DashboardCard({ result, getMyStudentTutorPairs }) {
 
       {/* remove from Dashboard Modal */}
       <div className={removeMessage}>
-        <div className="modal-background"></div>
-        <div className="modal-card">
-          <header className="modal-card-head modal-header-style">
-            <p className="modal-card-title">
+        <div className='modal-background'></div>
+        <div className='modal-card'>
+          <header className='modal-card-head modal-header-style'>
+            <p className='modal-card-title'>
               You are about to remove {result.firstName} {result.lastName} from
               your Dashboard
             </p>
             <button
-              className="delete"
-              aria-label="close"
+              className='delete'
+              aria-label='close'
               onClick={handleModalClose}
             ></button>
           </header>
-          <section className="modal-card-body">
+          <section className='modal-card-body'>
             <p>
               Are you sure you want to remove {result.firstName}{' '}
               {result.lastName} from your dashboard?
             </p>
           </section>
-          <footer className="modal-card-foot modal-bottom-style">
-            <button className="button is-info" onClick={removeFromDashboard}>
+          <footer className='modal-card-foot modal-bottom-style'>
+            <button className='button is-info' onClick={removeFromDashboard}>
               Yes, I'm sure
             </button>
-            <button className="button" onClick={handleModalClose}>
+            <button className='button' onClick={handleModalClose}>
               Cancel
             </button>
           </footer>
@@ -277,29 +303,33 @@ export default function DashboardCard({ result, getMyStudentTutorPairs }) {
 
       {/* read reviews Modal */}
       <div className={readReviewModal}>
-        <div className="modal-background"></div>
-        <div className="modal-card">
-          <header className="modal-card-head modal-header-style">
-            <p className="modal-card-title">
+        <div className='modal-background'></div>
+        <div className='modal-card'>
+          <header className='modal-card-head modal-header-style'>
+            <p className='modal-card-title'>
               Reviews for {result.firstName} {result.lastName}:
             </p>
             <button
-              className="delete"
-              aria-label="close"
+              className='delete'
+              aria-label='close'
               onClick={handleModalClose}
             ></button>
           </header>
-          <section className="modal-card-body">
-            {tutorReviews[0] ? tutorReviews.map((review) => (
-              <div key={review.createdAt} className="my-4 px-2">
-                <p>{review.review}</p>
-                <h3 className="is-pulled-right mr-6">-{review.reviewer}</h3>
-                <hr/>
-              </div>
-            )) : <p>There are currently no reviews for this tutor...</p>}
+          <section className='modal-card-body'>
+            {tutorReviews[0] ? (
+              tutorReviews.map((review) => (
+                <div key={review.createdAt} className='my-4 px-2'>
+                  <p>{review.review}</p>
+                  <h3 className='is-pulled-right mr-6'>-{review.reviewer}</h3>
+                  <hr />
+                </div>
+              ))
+            ) : (
+              <p>There are currently no reviews for this tutor...</p>
+            )}
           </section>
-          <footer className="modal-card-foot modal-bottom-style">
-            <button className="button" onClick={handleModalClose}>
+          <footer className='modal-card-foot modal-bottom-style'>
+            <button className='button' onClick={handleModalClose}>
               Close
             </button>
           </footer>
